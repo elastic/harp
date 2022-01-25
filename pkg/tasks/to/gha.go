29eb7af189547c56f30950beae4d4b1c779dfaa9
Thibault NORMAND <me@zenithar.org>
2022-01-25 17:13:14 +0100

    fix(lint): golanci warnings.

--- pkg/tasks/to/gha.go
+++ pkg/tasks/to/gha.go
@@ -26,12 +26,10 @@ import (
 	"os"
 
 	"github.com/google/go-github/v42/github"
-	"go.uber.org/zap"
 	"golang.org/x/crypto/nacl/box"
 	"golang.org/x/oauth2"
 
 	"github.com/elastic/harp/pkg/bundle"
-	"github.com/elastic/harp/pkg/sdk/log"
 	"github.com/elastic/harp/pkg/tasks"
 )
 
@@ -56,44 +54,17 @@ func (t *GithubActionTask) Run(ctx context.Context) error {
 		return fmt.Errorf("unable to load bundle: %w", err)
 	}
 
-	// Retrieve github token
-	githubToken := os.Getenv("GITHUB_TOKEN")
-	if githubToken == "" {
-		return errors.New("GITHUB_TOKEN environment variable must be set")
+	// Prepae github API client
+	client, err := t.prepareClient(ctx)
+	if err != nil {
+		return fmt.Errorf("unable to prepare github api client: %w", err)
 	}
-
-	// Create an authenticated transport
-	tc := oauth2.NewClient(
-		ctx,
-		oauth2.StaticTokenSource(
-			&oauth2.Token{
-				AccessToken: githubToken,
-			},
-		),
-	)
-
-	// Create github API client
-	client := github.NewClient(tc)
 
 	// Retrieve repository public key
-	pub, _, err := client.Actions.GetRepoPublicKey(ctx, t.Owner, t.Repository)
+	keyID, boxKey, err := t.getRepositoryKey(ctx, client)
 	if err != nil {
-		return fmt.Errorf("unable to retireve repository public key for secret encryption: %w", err)
+		return fmt.Errorf("unable to retieve repository public key: %w", err)
 	}
-
-	// Decode public key.
-	decodedPublicKey, err := base64.StdEncoding.DecodeString(pub.GetKey())
-	if err != nil {
-		return fmt.Errorf("unable to decode public key from github: %w", err)
-	}
-
-	// The decode key is converted into a fixed size byte array.
-	var boxKey [32]byte
-
-	// The secret value is converted into a slice of bytes.
-	copy(boxKey[:], decodedPublicKey)
-
-	log.For(ctx).Info("Repository public key feteched", zap.String("key_id", pub.GetKeyID()))
 
 	// Requests to send to github
 	githubSecrets := []*github.EncryptedSecret{}
@@ -115,7 +86,7 @@ func (t *GithubActionTask) Run(ctx context.Context) error {
 		filteredSecrets := secretMap.Glob(t.SecretFilter)
 
 		// Iterate over secrets
-		for key, value := range filteredSecrets {
+		for secretKey, value := range filteredSecrets {
 			var secretBytes []byte
 
 			// Pack the secret value
@@ -129,15 +100,15 @@ func (t *GithubActionTask) Run(ctx context.Context) error {
 			}
 
 			// The secret is encrypted with box.SealAnonymous using the repo's decoded public key.
-			encryptedBytes, err := box.SealAnonymous([]byte{}, secretBytes, &boxKey, rand.Reader)
+			encryptedBytes, err := box.SealAnonymous([]byte{}, secretBytes, boxKey, rand.Reader)
 			if err != nil {
 				return fmt.Errorf("unable to encrypt the secret payload: %w", err)
 			}
 
 			// Prepare the request
 			githubSecrets = append(githubSecrets, &github.EncryptedSecret{
-				Name:           key,
-				KeyID:          pub.GetKeyID(),
+				Name:           secretKey,
+				KeyID:          keyID,
 				EncryptedValue: base64.StdEncoding.EncodeToString(encryptedBytes),
 			})
 		}
@@ -147,10 +118,57 @@ func (t *GithubActionTask) Run(ctx context.Context) error {
 	for _, gs := range githubSecrets {
 		// Create or update the secret value.
 		if _, err := client.Actions.CreateOrUpdateRepoSecret(ctx, t.Owner, t.Repository, gs); err != nil {
-			return fmt.Errorf("unable to publish secret to github: %v", err)
+			return fmt.Errorf("unable to publish secret to github: %w", err)
 		}
 	}
 
 	// No error
 	return nil
+}
+
+func (t *GithubActionTask) prepareClient(ctx context.Context) (*github.Client, error) {
+	// Retrieve github token
+	githubToken := os.Getenv("GITHUB_TOKEN")
+	if githubToken == "" {
+		return nil, errors.New("GITHUB_TOKEN environment variable must be set")
+	}
+
+	// Create an authenticated transport
+	tc := oauth2.NewClient(
+		ctx,
+		oauth2.StaticTokenSource(
+			&oauth2.Token{
+				AccessToken: githubToken,
+			},
+		),
+	)
+
+	// Create github API client
+	client := github.NewClient(tc)
+
+	// No error
+	return client, nil
+}
+
+func (t *GithubActionTask) getRepositoryKey(ctx context.Context, client *github.Client) (keyID string, publicKey *[32]byte, err error) {
+	// Retrieve repository public key
+	pub, _, err := client.Actions.GetRepoPublicKey(ctx, t.Owner, t.Repository)
+	if err != nil {
+		return "", nil, fmt.Errorf("unable to retrieve repository public key for secret encryption: %w", err)
+	}
+
+	// Decode public key.
+	decodedPublicKey, err := base64.StdEncoding.DecodeString(pub.GetKey())
+	if err != nil {
+		return pub.GetKeyID(), nil, fmt.Errorf("unable to decode public key from github: %w", err)
+	}
+
+	// The decode key is converted into a fixed size byte array.
+	var boxKey [32]byte
+
+	// The secret value is converted into a slice of bytes.
+	copy(boxKey[:], decodedPublicKey)
+
+	// No error
+	return pub.GetKeyID(), &boxKey, nil
 }
