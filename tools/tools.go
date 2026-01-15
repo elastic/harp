5b96a66be3d464db37052204fb911faa8a99d79e
Ben Stickel <ben.stickel@elastic.co>
2026-01-14 17:01:04 -0800

    chore: update golangci-lint to v2
    - Update golangci-lint from v1.64.8 to v2.8.0
    - Migrate .golangci.yml to v2 configuration format
    - Update tools/tools.go import paths for v2
    - Remove deprecated linters (gofmt, goimports, gosimple, stylecheck)
    - Update depguard rules for v2 prefix matching
    
    Signed-off-by: Ben Stickel <ben.stickel@elastic.co>

--- tools/tools.go
+++ tools/tools.go
@@ -28,7 +28,7 @@ import (
 	_ "github.com/daixiang0/gci"
 	_ "github.com/frapposelli/wwhrd"
 	_ "github.com/golang/mock/mockgen"
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/golangci/golangci-lint/v2/cmd/golangci-lint"
 	_ "github.com/google/wire/cmd/wire"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
@@ -45,7 +45,7 @@ import (
 //go:generate go build -v -o=./bin/go-licenser github.com/elastic/go-licenser
 //go:generate go build -v -o=./bin/wwhrd github.com/frapposelli/wwhrd
 //go:generate go build -v -o=./bin/mockgen github.com/golang/mock/mockgen
-//go:generate go build -v -o=./bin/golangci-lint github.com/golangci/golangci-lint/cmd/golangci-lint
+//go:generate go build -v -o=./bin/golangci-lint github.com/golangci/golangci-lint/v2/cmd/golangci-lint
 //go:generate go build -v -o=./bin/wire github.com/google/wire/cmd/wire
 //go:generate go build -v -o=./bin/protoc-gen-go-grpc google.golang.org/grpc/cmd/protoc-gen-go-grpc
 //go:generate go build -v -o=./bin/protoc-gen-go google.golang.org/protobuf/cmd/protoc-gen-go
